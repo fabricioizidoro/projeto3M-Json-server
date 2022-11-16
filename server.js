2a40d5322e8bb51111a4c37f63fd367c70bd88c2
fabricioizidoro <izidoro.ferreira163@gmail.com>
2022-11-15 21:11:59 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const js = require('json-server');
+const create = js.create();
+const router = js.router("db.json");
+const defaults = js.defaults();
+
+
+create.use(defaults);
+create.use(js.bodyParser);
+
+create.use(router)
+
+create.listen(3000,() => {console.log("parábens, funcionou!")});
